f34c821323b737cb36e866bae74d533213affa50
Mohamed Asif <farookasif007@gmail.com>
2021-10-04 16:47:39 +0530

    initial commit

---
+++ src/main/java/seleniumTest/OpenGoogle.java
@@ -0,0 +1,14 @@
+package seleniumTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.co.in");
+	}
+
+}
